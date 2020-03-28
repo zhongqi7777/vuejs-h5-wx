39cdd9dd4127a6012ad4397db1aa71e57fd6e541
yanlibo2021 <yanlibo2021@126.com>
2020-03-28 22:49:54 +0800

    add src

---
+++ src/components/base/ImageView.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="image-view" @click="onClick" :style="{ height }">
+    <img
+      :class="round ? 'round image' : 'image'"
+      :style="{ height }"
+      :src="src"
+      :mode="mode"
+      :lazy-load="lazyLoad"
+      @load="onLoad"
+      @error="onError"
+      v-show="!isLoading && !error"
+    >
+    <img
+      :class="round ? 'round image' : 'image'"
+      :style="{ height }"
+      src="https://www.youbaobao.xyz/book/img/loading2.ae9e5924.jpeg"
+      :mode="mode"
+      :lazy-load="lazyLoad"
+      v-show="isLoading || error"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ImageView',
+    props: {
+      src: {
+        type: String,
+        default: ''
+      },
+      mode: {
+        type: String,
+        default: 'widthFix'
+      },
+      lazyLoad: {
+        type: Boolean,
+        default: true
+      },
+      round: {
+        type: Boolean,
+        default: false
+      },
+      height: {
+        type: String,
+        default: 'auto'
+      }
+    },
+    watch: {
+      src(newValue, preValue) {
+      }
+    },
+    data() {
+      return {
+        isLoading: true,
+        error: false
+      }
+    },
+    methods: {
+      onClick() {
+        this.$emit('onClick')
+      },
+      onLoad() {
+        this.isLoading = false
+        this.error = false
+      },
+      onError() {
+        this.error = true
+        this.isLoading = false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .image-view
+    width: 100%
+  .image
+    width: 100%
+  &.round
+     border-radius: 50%
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="home">
+    <!-- <searchBar disabled @onClick="onSearchBarkClick" />
+    <HomeCard />
+    <HomeBanner
+      img="http://www.youbaobao.xyz/book/res/bg.jpg"
+      title="mpvue2.0实战多端小程序课程上线啦"
+      subTitle="立即体验"
+      @onClick="onBannerClick"
+    />
+    <div :style="{marginTop: '23px'}">
+      <HomeBook
+        title="为你推荐"
+        :row="2"
+        :col="2"
+        :data="data"
+        mode="category"
+        btn-text="换一批"
+        @onMoreClick="onBookMoreClick"
+        @onBookClick="onHomeBookClick"
+      />
+    </div>-->
+    <h1>h5 小程序</h1>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+// import SearchBar from "../../components/home/SearchBar";
+// import HomeCard from "../../components/home/HomeCard";
+// import HomeBanner from "../../components/home/HomeBanner";
+// import HomeBook from "../../components/home/HomeBook";
+import { get } from "../../utils/request";
+
+export default {
+  components: {
+    // HomeBook,
+    // HomeBanner,
+    // HomeCard,
+    // SearchBar
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    //console.log(this.getHomeData());
+  },
+  methods: {
+    getHomeData() {
+      get("https://test.youbaobao.xyz:18081/book/home/v2?openId=1234").then(
+        response => {
+          console.log(response);
+        }
+      );
+    },
+    onBookMoreClick() {
+      console.log("book click");
+    },
+    onHomeBookClick() {
+      console.log("more click");
+    },
+    // 搜索跳转页
+    onSearchBarkClick() {},
+    onBannerClick() {}
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.book-home {
+  margin-top: 23px;
+}
+</style>
