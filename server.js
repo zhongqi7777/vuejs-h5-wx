0621fec4580c9417f24a72eef90d1e95e0330ea1
yanlibo2021 <yanlibo2021@126.com>
2020-04-04 20:17:26 +0800

    modified:   package.json         modified:   server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'distH5')));
 const port = process.env.PORT || 80;
 app.listen(port);
