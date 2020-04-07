7ac885a6aa6bd21a971b2198a2be244a138a4707
yanlibo2021 <yanlibo2021@126.com>
2020-04-07 15:49:15 +0800

    modified:   src/App.vue         modified:   src/AppH5.vue         modified:   src/mainH5.js         renamed:    src/api/httpService.js -> src/utils/httpService.js

--- src/utils/httpservice/h5.js
+++ src/utils/httpservice/h5.js
@@ -1,12 +1,18 @@
 // import Fly from 'flyio/dist/npm/wx'
 // 'flyio'(与axios、fentch对比 https://wendux.github.io/dist/#/doc/flyio/compare)
 var Fly = require("flyio") //npm引入方式
+import { Toast } from "vant";
 
 const request = new Fly()
 
 request.interceptors.request.use((request) => {
-    wx.showLoading({ title: '加载中..' })
-    // wx.showNavigationBarLoading() //显示导航条加载动画。
+    // 自定义加载图标
+    Toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        loadingType: 'spinner',
+        duration: 0
+    });
     return request
 })
 
@@ -14,15 +20,18 @@ request.interceptors.response.use(
     (response, promise) => {
         wx.hideLoading()
         // wx.hideNavigationBarLoading()
+        Toast.clear();
         return promise.resolve(response.data)
     },
     (err, promise) => {
         // wx.hideNavigationBarLoading()
-        wx.showToast({
-            title: err.message,
-            icon: 'none',
-            duration: 1000
-        })
+        // wx.showToast({
+        //     title: err.message,
+        //     icon: 'none',
+        //     duration: 1000
+        // })
+
+        Toast.fail(err.message);
         return promise.resolve()
     }
 )
