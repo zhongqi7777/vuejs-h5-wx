eed751762a1e7fc5417b92a8a45ab3935ef05015
yanlibo2021 <yanlibo2021@126.com>
2020-04-08 19:36:04 +0800

    modified:   build/webpack.dev.conf.js         modified:   buildH5/webpack.devH5.conf.js         modified:   configH5/dev.env.js         new file:   mock/index.js         new file:   mock/mock-server.js         new file:   mock/remote-search.js         new file:   mock/user.js         modified:   package.json         modified:   src/main.js         modified:   src/mainH5.js         modified:   src/utils/index.js         modified:   yarn.lock

--- configH5/prod.env.js
+++ configH5/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  platform_h5: JSON.stringify(process.env.npm_config_platform),
+  mpvuePlatform: JSON.stringify(process.env.npm_config_platform),
   BASE_ENV: JSON.stringify(process.env.npm_config_baseenv) || '"mock"',
 }
