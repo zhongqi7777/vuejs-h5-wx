a8fc382603d4f8f487e9d6f00e8e122ea31deff5
zhongqi7777 <yanlibo@300.cn>
2020-06-15 17:09:41 +0800

    modified:   src/pages/home/h5/index.vue         modified:   src/pages/home/mp/index.vue

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,3 @@
-// import h5 from "./entry/h5";
-// import wx from "./entry/wx";
-
-// const getProjectName = val => {
-//   switch (val) {
-//     case "h5":
-//       return h5;
-//     case "wx":
-//       return wx;
-//     default:
-//     //return h5;
-//   }
-// };
-
-// let result = process.env.mpvuePlatform || mpvuePlatform;
-// export default getProjectName(result);
-
 import Vue from "vue";
 import Vuex from "vuex";
 import getters from "./getters";
