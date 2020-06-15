ce1dcd32716c3498c26f9b643613802059211cb1
zhongqi7777 <yanlibo@300.cn>
2020-06-15 17:55:08 +0800

    modified:   .postcssrc.js         modified:   package.json

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,22 @@
 // // https://github.com/michael-ciniawsky/postcss-load-config
 
-module.exports = process.env.npm_config_platform === 'h5' ? {
-  "plugins": {
-  }
-} : {
-    "plugins": {
-      "postcss-mpvue-wxss": {}
-    }
-  }
+module.exports =
+  process.env.npm_config_platform === "h5"
+    ? {
+        plugins: {
+          // autoprefixer: {
+          //   overrideBrowserslist: [
+          //     "Android 4.1",
+          //     "iOS 7.1",
+          //     "Chrome > 31",
+          //     "ff > 31",
+          //     "ie >= 8"
+          //   ]
+          // }
+        }
+      }
+    : {
+        plugins: {
+          "postcss-mpvue-wxss": {}
+        }
+      };
