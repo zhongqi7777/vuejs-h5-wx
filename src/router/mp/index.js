1ca8e387fefe9dc56dd0b5891bf7f38c092e41f8
zhongqi7777 <yanlibo@300.cn>
2020-06-15 17:07:51 +0800

    modified:   src/pages/home/h5/index.vue         modified:   src/pages/home/mp/index.vue

--- src/router/mp/index.js
+++ src/router/mp/index.js
@@ -3,7 +3,7 @@ import Vue from "vue";
 import VueRouter from "@/utils/vue-router-mp/src/index.js";
 Vue.use(VueRouter);
 
-import store from "@/store/entry/wx";
+import store from "@/store/index";
 
 // 1. 定义路由
 // 此处由于小程序页面路径与源码一一对应，故无需配置组件
@@ -11,50 +11,50 @@ const routes = [
   {
     name: "home", // 命名路由
     path: "/pages/home/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
   {
     name: "category", // 命名路由
     path: "/pages/category/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
   {
     name: "cart", // 命名路由
     path: "/pages/cart/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
   {
     name: "eat", // 命名路由
     path: "/pages/eat/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
   {
     name: "mine", // 命名路由
     path: "/pages/mine/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
   {
     name: "goods", // 命名路由
     path: "/pages/goods/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
   {
     name: "order", // 命名路由
     path: "/pages/order/mp/main",
-    isTab: false, // 标记当前路由是一个 Tab
+    isTab: false // 标记当前路由是一个 Tab
   },
 
   {
     name: "/pages/demo/mp/index/main", // 命名路由
     path: "/pages/demo/mp/index/main",
-    isTab: false, // 标记当前路由是一个 Tab
-  },
+    isTab: false // 标记当前路由是一个 Tab
+  }
 ];
 
 // 2. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-  routes, // (缩写) 相当于 routes: routes
+  routes // (缩写) 相当于 routes: routes
 });
 
 router.beforeEach((to, from, next) => {
